b86ab579a14d2c8ab1c40e018d6d773ef7b55ef5
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-09-27 22:41:33 +0200

    update

--- java_understanding/m/src/main/java/package_2/how_to_test.java
+++ java_understanding/m/src/main/java/package_2/how_to_test.java
@@ -1,25 +1,48 @@
 package package_2;
 
 public class how_to_test {
+
+    // * Vi skal lave methods her, baseret på test.
+
+    /*
+     * // TODO TEST:[ VOID TEXT OUTPUT, int input output, text input output, Liste
+     * med ting, validation med liste af elementer at teste for(Strings), validation
+     * med liste af elementer at teste for(ints) ]
+     * 
+     * 
+     * 
+     */
+
+    public void void_method() {
+        System.out.println("Hello world!");
+    }
+
+    public String how_to_return_a_string() {
+        String myString = "string indehold!";
+        return myString;
+    }
+
+    public int how_to_return_a_int() {
+        int myint = 69;
+        return myint;
+    }
+
+    public int[] how_to_return_a_array() {
+        int[] myIntArray = { 1, 2, 3 };
+        return myIntArray;
+    }
+
+    public String[] how_to_return_a_string_array() {
+        String[] myStringArray = { "Yes", "This", "how", "you", "do", "it!" };
+        return myStringArray;
+    }
+
+    public boolean how_to_return_a_bool(boolean mybool){
+        mybool = false;
+        return mybool;
+    }
+
     
-// * Vi skal lave methods her, baseret på test.
-
-/*// TODO TEST:[ VOID TEXT OUTPUT, int input output,  text input output, Liste med ting,   validation med liste af elementer at teste for(Strings),  validation med liste af elementer at teste for(ints) ]
- * 
- * 
- * 
- */
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ java_understanding/m/src/test/java/package_2/how_to_testTest.java
@@ -0,0 +1,38 @@
+package package_2;
+
+import org.junit.Test;
+
+public class how_to_testTest {
+
+
+    
+    @Test
+    public void testHow_to_return_a_array() {
+
+    }
+
+    @Test
+    public void testHow_to_return_a_bool() {
+
+    }
+
+    @Test
+    public void testHow_to_return_a_int() {
+
+    }
+
+    @Test
+    public void testHow_to_return_a_string() {
+
+    }
+
+    @Test
+    public void testHow_to_return_a_string_array() {
+
+    }
+
+    @Test
+    public void testVoid_method() {
+
+    }
+}
