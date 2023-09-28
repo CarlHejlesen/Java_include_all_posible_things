b9fceba8682c8a0da5cad730ba912a59320b64fc
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-09-28 11:47:37 +0200

    update

--- java_understanding/m/src/test/java/package_2/how_to_testTest.java
+++ java_understanding/m/src/test/java/package_2/how_to_testTest.java
@@ -10,7 +10,20 @@ public class how_to_testTest {
     public void testHow_to_return_a_array() {
 
     }
+     
 
+    /*Jeg ønsker at denne test skal skal kunne validere 5 forskellige strings
+     * Den skal teste for om dit input er en string.
+     * Den skal teste for om dit string er over 2 bogstaver
+     * Den skal teste for at dit string ikke er over 50 bogstaver
+     * Den skal teste for at det første bogstav er stort
+     * 
+     * 
+     * Den skal retunere lige så mange bools som den modtager.
+     * Vi skal sammenligne vores input med det output bools. Vi ved hvad den burde returnere
+     * 
+     */
+    
     @Test
     public void testHow_to_return_a_bool() {
 
