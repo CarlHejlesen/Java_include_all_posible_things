f7163dab008cf1ad36defcb89aaa33c07a947c21
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-10-21 15:31:50 +0200

    Pom -auto_run

--- techstack/nativeReact_Java_MySQL/back-end/src/main/java/Main.java
+++ techstack/nativeReact_Java_MySQL/back-end/src/main/java/Main.java
@@ -1,18 +1,29 @@
 import java.sql.Connection;
+import java.sql.SQLException;
 
-import mySQL_handle.connect_to_server;
+import mySQL_handle.return_connection_to_server;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-
-        connect_to_server connect_to_server_instance = new connect_to_server();
-        Connection connection = connect_to_server_instance.getConnection();
-        if (connection != null) {
-            System.out.println("Forbindelse oprettet!");
-        } else {
-            System.out.println("Kunne ikke oprette forbindelse til databasen.");
+        Connection connection = null;
+        try {
+            return_connection_to_server connection_to_server_instance = new return_connection_to_server();
+             connection = connection_to_server_instance.getConnection();
+            if (connection != null) {
+                System.out.println("Forbindelse oprettet!");
+            } else {
+                System.out.println("Kunne ikke oprette forbindelse til databasen.");
+            }
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                    System.out.println("Forbindelse lukket!");
+                } catch (SQLException e) {
+                    System.err.println("Fejl ved lukning af forbindelse: " + e.getMessage());
+                }
+            }
         }
-
     }
 }
--- techstack/nativeReact_Java_MySQL/back-end/src/main/java/mySQL_handle/return_connection_to_server.java
+++ techstack/nativeReact_Java_MySQL/back-end/src/main/java/mySQL_handle/return_connection_to_server.java
@@ -2,7 +2,7 @@ package mySQL_handle;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-public class connect_to_server {
+public class return_connection_to_server {
     
 
 private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/";
