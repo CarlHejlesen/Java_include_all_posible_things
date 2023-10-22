9ba716a1dad7b1e9eff97f11897e5227f8daa376
CarlHejlesen <113053807+CarlHejlesen@users.noreply.github.com>
2023-10-22 19:46:32 +0200

    commetare til getdata

--- techstack/nativeReact_Java_MySQL/back-end/src/main/java/mySQL_handle/GetFirstDataInSkema.java
+++ techstack/nativeReact_Java_MySQL/back-end/src/main/java/mySQL_handle/GetFirstDataInSkema.java
@@ -19,18 +19,19 @@ public class GetFirstDataInSkema {
     /**
      * Fetches the age of a person based on their name from a specified table.
      * 
-     * @param name Name of the person to fetch the age for.
+     * @param name      Name of the person to fetch the age for.
      * @param tablename The name of the database table to search within.
      * @return Age of the person. Returns -1 if not found or an error occurs.
      */
     public int getAgeByName(String name, String tablename) {
-        // Create the SQL query string to fetch the age of the person based on their name from the specified table.
+        // Create the SQL query string to fetch the age of the person based on their
+        // name from the specified table.
         String query = "SELECT age FROM " + tablename + " WHERE name = '" + name + "'";
-        
+
         try (PreparedStatement statement = connection.prepareStatement(query)) {
             // Execute the SQL query.
             ResultSet resultSet = statement.executeQuery();
-    
+
             // If a result is found, return the age.
             if (resultSet.next()) {
                 return resultSet.getInt("age");
@@ -39,9 +40,8 @@ public class GetFirstDataInSkema {
             // Print an error message if there's an exception during the SQL operation.
             System.err.println("Error fetching age: " + e.getMessage());
         }
-        
+
         // If no result is found or an error occurs, return -1.
         return -1;
     }
 }
-
